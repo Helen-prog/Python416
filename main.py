c88e3e8d5cb5ee97f1e699d8eef10847265107e0
Helen <kozovyakina@gmail.com>
2025-04-03 21:59:36 +0300

    ooп

--- main.py
+++ main.py
@@ -3745,39 +3745,216 @@
 #
 # print(Point.__dict__)
 
-def func():
-    print("текст")
-
-
-func()
-
-
-class Point:
-    """Класс для предоставления координат точек на плоскости"""
-    x = 1
-    y = 2
-
-    def set_coord(self, x1, y1):
-        self.x = x1
-        self.y = y1
-
-
-p1 = Point()  # экземпляр класса (объект)
-p1.set_coord(5, 3)
-print(p1.__dict__)
-# print(Point.__doc__)
-# print(Point.__dict__)
-print(type(p1))
-print(type(5))
-# p1.x = 5
-# p1.y = 10
-# p1.set_coord(5, 10)
+# class Point:
+#     """Класс для предоставления координат точек на плоскости"""
+#     x = 1
+#     y = 2
+#
+#     def set_coord(self, x1, y1):
+#         self.x = x1
+#         self.y = y1
+#
+#
+# p1 = Point()  # экземпляр класса (объект)
+# p1.set_coord(5, 3)
 # print(p1.__dict__)
-# print(p1.x)
-# # Point.set_coord(p1, 20, 30)
+# # print(Point.__doc__)
+# # print(Point.__dict__)
+# print(type(p1))
+# print(type(5))
+# # p1.x = 5
+# # p1.y = 10
+# # p1.set_coord(5, 10)
 # # print(p1.__dict__)
-# #
-p2 = Point()
-p2.set_coord(100, 200)
-# print(p2.__dict__)
+# # print(p1.x)
+# # # Point.set_coord(p1, 20, 30)
+# # # print(p1.__dict__)
+# # #
+# p2 = Point()
+# p2.set_coord(100, 200)
+# # print(p2.__dict__)
 # print(p2.x)
+
+class Human:
+    name = "name"
+    birthday = "00.00.0000"
+    phone = "00-00-00"
+    country = "country"
+    city = "city"
+    address = "street, house"
+
+    def print_info(self):
+        print(" Персональные данные ".center(40, "*"))
+        print(f"Имя: {self.name}\nДата рождения: {self.birthday}\n"
+              f"Номер телефона: {self.phone}\nСтрана: {self.country}\n"
+              f"Город: {self.city}\nДомашний адрес: {self.address}")
+        print("=" * 40)
+
+    def input_info(self, first_name, birthday, phone, country, city, address):
+        self.name = first_name
+        self.birthday = birthday
+        self.phone = phone
+        self.country = country
+        self.city = city
+        self.address = address
+
+    def set_name(self, name):  # устанавливаем новое имя
+        self.name = name
+
+    def get_name(self):  # получаем имя
+        return self.name
+
+
+h1 = Human()
+h1.print_info()
+h1.input_info("Юля", "23.05.1986", "45-46-98", "Россия", "Москва", "Чистопрудный бульвар, 1A")
+h1.print_info()
+h1.set_name("Юлия")
+h1.print_info()
+print(h1.get_name())
+
+# class Person:
+#     skill = 10
+#
+#     def __init__(self, name, surname):  # инициализатор
+#         self.name = name
+#         self.surname = surname
+#         # print("Инициализатор для", self.name, self.surname)
+#
+#     def __del__(self):
+#         print("Удаление экземпляра")
+#
+#     def print_info(self):
+#         print("Данные сотрудника:", self.name, self.surname)
+#
+#     def add_skill(self, k):
+#         self.skill += k
+#         print("Квалификация сотрудника:", self.skill, "\n")
+#
+#
+# p1 = Person("Виктор", "Резник")
+# p1.print_info()
+# p1.add_skill(3)
+#
+# del p1
+# print()
+#
+# p2 = Person("Анна", "Долгих")
+# p2.print_info()
+# p2.add_skill(2)
+
+# class Point:
+#     count = 0
+#
+#     def __init__(self, x=0, y=0):
+#         self.x = x
+#         self.y = y
+#         Point.count += 1
+#
+#
+# p1 = Point()
+# p2 = Point()
+# p3 = Point()
+# print(Point.count)
+
+
+# class Robot:
+#     k = 0
+#
+#     def __init__(self, name):
+#         self.name = name
+#         print("Инициализация робота:", self.name)
+#         Robot.k += 1
+#
+#     def __del__(self):
+#         print(self.name, "выключается!")
+#         Robot.k -= 1
+#
+#         if Robot.k == 0:
+#             print(self.name, "был последним")
+#         else:
+#             print("Работающих роботов осталось:", Robot.k)
+#
+#     def say_hi(self):
+#         print("Приветствую! Меня зовут:", self.name)
+#
+#
+# droid1 = Robot("R2-D2")
+# droid1.say_hi()
+# print("Численность роботов:", Robot.k)
+#
+# droid2 = Robot("C-3PO")
+# droid2.say_hi()
+# print("Численность роботов:", Robot.k)
+#
+# droid3 = Robot("PC-3O")
+# droid3.say_hi()
+# print("Численность роботов:", Robot.k)
+#
+# print("\nЗдесь роботы могут проделать какую-то работу\n")
+# print("Роботы закончили свою работу. Давайте их выключим.")
+#
+# del droid1
+# del droid2
+# del droid3
+#
+# print("Численность роботов:", Robot.k)
+
+
+# class Point:
+#
+#     def __init__(self, x, y):
+#         self.__x = self.__y = 0
+#         if Point.__check_value(x) and Point.__check_value(y):
+#             self.__x = x
+#             self.__y = y
+#
+#     def __check_value(s):
+#         if isinstance(s, int) or isinstance(s, float):
+#             return True
+#         return False
+#
+#     def set_coord(self, x, y):
+#         if Point.__check_value(x) and Point.__check_value(y):
+#             self.__x = x
+#             self.__y = y
+#         else:
+#             print("Координаты должны быть числами")
+#
+#     def set_coord_x(self, x):
+#         if Point.__check_value(x):
+#             self.__x = x
+#         else:
+#             print("Координаты должны быть числами")
+#
+#     def set_coord_y(self, y):
+#         if Point.__check_value(y):
+#             self.__y = y
+#         else:
+#             print("Координаты должны быть числами")
+#
+#     def get_coord(self):
+#         return self.__x, self.__y
+#
+#     def get_coord_x(self):
+#         return self.__x
+#
+#     def get_coord_y(self):
+#         return self.__y
+#
+#
+# p1 = Point(5, "10")
+# # print(p1.__dict__)
+# # p1.z = 20
+# # print(p1.__x, p1.__y)
+# # p1.__x = 50
+# # p1.__y = "abc"
+# p1.set_coord(5.2, 100)
+# print(p1.get_coord())
+# p1.set_coord_x(10)
+# p1.set_coord_y(50)
+#
+# p1._Point__x = "abc"
+# # print(p1._Point__x)
+# print(p1.__dict__)
+
