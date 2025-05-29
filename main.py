c68617b52751d1f9499742a76c1593f6b793ea98
Helen <kozovyakina@gmail.com>
2025-05-29 21:26:24 +0300

    db

--- main.py
+++ main.py
@@ -6572,7 +6572,7 @@ import csv
 # if __name__ == '__main__':
 #     run()
 
-from jinja2 import Template
+# from jinja2 import Template
 
 # name = "Игорь"
 # age = 28
@@ -6609,30 +6609,128 @@ from jinja2 import Template
 #
 # print(msg)
 
+#
+# cities = [
+#     {'id': 1, 'city': 'Москва'},
+#     {'id': 2, 'city': 'Смоленск'},
+#     {'id': 3, 'city': 'Минск'},
+#     {'id': 4, 'city': 'Сочи'},
+#     {'id': 5, 'city': 'Ярославль'},
+# ]
+#
+# link = """
+# <select>
+# {% for c in cities %}
+#     {% if c.id > 3 %}
+#         <option value="{{ c['id'] }}">{{ c['city'] }}</option>
+#     {% elif c.city == 'Москва' %}
+#         <option>{{ c['city'] }}</option>
+#     {% else %}
+#         {{ c['city'] }}
+#     {% endif %}
+# {% endfor %}
+# </select>
+# """
+#
+# tm = Template(link)
+# msg = tm.render(cities=cities)
+#
+# print(msg)
 
-cities = [
-    {'id': 1, 'city': 'Москва'},
-    {'id': 2, 'city': 'Смоленск'},
-    {'id': 3, 'city': 'Минск'},
-    {'id': 4, 'city': 'Сочи'},
-    {'id': 5, 'city': 'Ярославль'},
-]
 
-link = """
-<select>
-{% for c in cities %}
-    {% if c.id > 3 %}
-        <option value="{{ c['id'] }}">{{ c['city'] }}</option>
-    {% elif c.city == 'Москва' %}    
-        <option>{{ c['city'] }}</option>
-    {% else %}
-        {{ c['city'] }}
-    {% endif %}
-{% endfor %}
-</select>
-"""
+# menu = [
+#     {'href': '/index', 'link': 'Главная'},
+#     {'href': '/news', 'link': 'Новости'},
+#     {'href': '/about', 'link': 'О компании'},
+#     {'href': '/shop', 'link': 'Магазин'},
+#     {'href': '/contacts', 'link': 'Контакты'},
+# ]
+#
+# link = """
+# <ul>
+#     {% for i in menu %}
+#         {% if i.link == 'Главная' %}
+#             <li><a href="{{ i['href'] }}" class="active">{{ i['link'] }}</a></li>
+#         {% else %}
+#             <li><a href="{{ i['href'] }}">{{ i['link'] }}</a></li>
+#         {% endif %}
+#     {% endfor %}
+# </ul>
+# """
+#
+# tm = Template(link)
+# msg = tm.render(menu=menu)
+#
+# print(msg)
 
-tm = Template(link)
-msg = tm.render(cities=cities)
+# cars = [
+#     {"model": 'Audi', 'price': 23000},
+#     {"model": 'Skoda', 'price': 17300},
+#     {"model": 'Renault', 'price': 44300},
+#     {"model": 'Wolksvagen', 'price': 21300}
+# ]
+# # cars = [3, 5, 7]
+#
+# # tpl = "{{ cs | sum(attribute='price') }}"
+# # tpl = "{{ (cs | max(attribute='price')).model }}"
+# tpl = "{{ (cs|min(attribute='price')).model }}"
+#
+# tm = Template(tpl)
+# msg = tm.render(cs=cars)
+#
+# print(msg)
 
-print(msg)
+# html = """
+# {% macro set_input(name, value='', type='text', size=20) %}
+#     <input type="{{ type }}" name="{{ name }}" value="{{ value }}" size="{{ size }}">
+# {% endmacro %}
+#
+# <p>{{ set_input('username') }}</p>
+# <p>{{ set_input('email') }}</p>
+# <p>{{ set_input('password', '', 'password') }}</p>
+# """
+#
+# tm = Template(html)
+# msg = tm.render()
+#
+# print(msg)
+
+# from jinja2 import Environment, FileSystemLoader
+#
+# persons = [
+#     {"name": "Алексей"},
+#     {"name": "Никита"},
+#     {"name": "Виталий"},
+# ]
+#
+# file_loader = FileSystemLoader('templates')
+# env = Environment(loader=file_loader)
+#
+# tm = env.get_template('about.html')
+# msg = tm.render(users=persons, title="About Jinja")
+#
+# print(msg)
+
+import sqlite3
+
+# con = sqlite3.connect("profile.db")
+# cur = con.cursor()
+#
+# cur.execute("")
+#
+# con.close()
+
+with sqlite3.connect("profile.db") as con:
+    cur = con.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    summa REAL,
+    data TEXT 
+    )""")
+    cur.execute("DROP TABLE users")
+
+
+
+
+
