7b5484d619f4fd4e71b6869d2c950a78f78c3677
Helen <kozovyakina@gmail.com>
2025-06-03 21:31:07 +0300

    db

--- main.py
+++ main.py
@@ -6709,28 +6709,61 @@ import csv
 # tm = env.get_template('about.html')
 # msg = tm.render(users=persons, title="About Jinja")
 #
-# print(msg)
+# # print(msg)
+#
+# import sqlite3
+#
+# # con = sqlite3.connect("profile.db")
+# # cur = con.cursor()
+# #
+# # cur.execute("")
+# #
+# # con.close()
+#
+# with sqlite3.connect("profile.db") as con:
+#     cur = con.cursor()
+#     cur.execute("""CREATE TABLE IF NOT EXISTS users(
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     name TEXT NOT NULL,
+#     summa REAL,
+#     data TEXT
+#     )""")
+#     cur.execute("DROP TABLE users")
+
 
 import sqlite3
 
-# con = sqlite3.connect("profile.db")
-# cur = con.cursor()
-#
-# cur.execute("")
-#
-# con.close()
-
-with sqlite3.connect("profile.db") as con:
+with sqlite3.connect("users.db") as con:
     cur = con.cursor()
-    cur.execute("""CREATE TABLE IF NOT EXISTS users(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    summa REAL,
-    data TEXT 
-    )""")
-    cur.execute("DROP TABLE users")
+    # cur.execute("""CREATE TABLE IF NOT EXISTS users(
+    # id INTEGER PRIMARY KEY AUTOINCREMENT,
+    # name TEXT NOT NULL,
+    # phone BLOB NOT NULL DEFAULT "+7 909 1234567",
+    # age INTEGER CHECK(age > 0 AND age < 100),
+    # email TEXT UNIQUE
+    # )""")
 
+    # # переименование таблицы
+    # cur.execute("""
+    # ALTER TABLE users
+    # RENAME TO person_table;
+    # """)
 
+    # # добавление нового столбца
+    # cur.execute("""
+    # ALTER TABLE person_table
+    # ADD COLUMN address TEXT;
+    # """)
 
+    # # Переименование столбца
+    # cur.execute("""
+    # ALTER TABLE person_table
+    # RENAME COLUMN address TO home_address;
+    # """)
 
-
+    # # удаление столбца
+    # cur.execute("""
+    #     ALTER TABLE person_table
+    #     DROP COLUMN home_address;
+    #     """)
+   
