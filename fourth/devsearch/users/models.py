9bc279730e8df5729b8af3dd51473ba5a459716f
Helen <kozovyakina@gmail.com>
2025-07-24 21:26:54 +0300

    devsearch

--- fourth/devsearch/users/models.py
+++ fourth/devsearch/users/models.py
@@ -27,3 +27,4 @@ class Skill(models.Model):
 
     def __str__(self):
         return self.name
+
--- fourth/devsearch/users/views.py
+++ fourth/devsearch/users/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Profile
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+
+
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('profiles')
+
+    if request.method == "POST":
+        username = request.POST['username'].lower()
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except ObjectDoesNotExist:
+            print('Username does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            print("Username or password is incorrect")
+
+    return render(request, 'users/login_register.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
 
 
 def profiles(request):
@@ -8,3 +41,18 @@ def profiles(request):
         'profiles': prof
     }
     return render(request, 'users/index.html', context)
+
+
+def user_profile(request, pk):
+    prof = Profile.objects.get(id=pk)
+
+    top_skills = prof.skill_set.exclude(description__exact="")
+    other_skills = prof.skill_set.filter(description="")
+
+    context = {
+        'profile': prof,
+        'top_skills': top_skills,
+        'other_skills': other_skills,
+    }
+
+    return render(request, 'users/profile.html', context)
