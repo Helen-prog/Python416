ad47dda7f1c779d7119731dc6239839c26c364a2
Helen <kozovyakina@gmail.com>
2025-08-21 21:34:00 +0300

    auth

--- fourth/devsearch/users/forms.py
+++ fourth/devsearch/users/forms.py
@@ -1,5 +1,32 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import ModelForm
+from .models import Profile, Skill
+
+
+class SkillForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for fields in self.fields.values():
+            fields.widget.attrs.update({'class': 'input'})
+
+    class Meta:
+        model = Skill
+        fields = '__all__'
+        exclude = ['owner']
+
+
+class ProfileForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for fields in self.fields.values():
+            fields.widget.attrs.update({'class': 'input'})
+
+    class Meta:
+        model = Profile
+        fields = ['name', 'email', 'username', 'bio', 'short_intro', 'profile_image', 'social_github', 'social_youtube', 'social_website']
 
 
 class CustomUserCreationForm(UserCreationForm):
